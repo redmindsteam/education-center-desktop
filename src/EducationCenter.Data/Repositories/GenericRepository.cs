dd160c2f01fc8a399f0775a6abc0361ec646f52b
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-09-09 09:00:18 +0500

    Created Common Interface and Designed Repositories

---
+++ src/EducationCenter.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using EducationCenter.Data.Interfaces;
+using System.Linq.Expressions;
+
+namespace EducationCenter.Data.Repositories;
+
+public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    public Task<T> CreateAsync(Task entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> FindAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> UpdateAsync(long id, T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IQueryable<T>> WhereAsync(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/EducationCenter.Domain/Constants/DbConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace EducationCenter.Domain.Constants;
+
+public class DbConstants
+{
+    public const string CONNECTION_STRING = "Host=localhost; port=5432; database=edu-center-crm-db; User Id = postgres; Password=785214;";
+}
