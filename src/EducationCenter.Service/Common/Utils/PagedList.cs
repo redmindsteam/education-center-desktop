665e5ea1548b26ed45008eff98488556779f3fff
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-09-10 13:31:24 +0500

    Complated Paginations

---
+++ src/EducationCenter.Service/Common/Utils/PagedList.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EducationCenter.Service.Common.Utils;
+
+public class PagedList<T> : List<T>
+{
+    public int CurrentPage { get; private set; }
+    public int TotalPages { get; private set; }
+    public int PageSize { get; private set; }
+    public int TotalCount { get; private set; }
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
+    public PagedList(List<T> items, int count,
+        int pageNumber, int pageSize)
+    {
+        TotalCount = count;
+        PageSize = pageSize;
+        CurrentPage = pageNumber;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        AddRange(items);
+    }
+    public async static Task<PagedList<T>> ToPagedListAsync(
+        IQueryable<T> source, int pageNumber, int pageSize)
+    {
+        var count = await source.CountAsync();
+        var items = await source.Skip(
+            (pageNumber - 1) * pageSize).Take(pageSize)
+            .ToListAsync();
+        return new PagedList<T>(items, count, pageNumber, pageSize);
+    }
+}
